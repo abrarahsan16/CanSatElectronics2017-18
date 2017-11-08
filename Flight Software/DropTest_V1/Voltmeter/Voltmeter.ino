bda28862847b96afa746a2f8fba7708f5e9b945e
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2017-11-07 22:57:47 -0500

    Voltmeter

---
+++ Flight Software/DropTest_V1/Voltmeter/Voltmeter.ino
@@ -0,0 +1,19 @@
+float vout; //Voltage being read by the arduino
+float vin; //Voltage being supplied
+
+float r1=100; //Change according to the value of the first resistor
+float r2=100; //Change according to the value of the second resistor
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);  //This gives the exact reading from the analogue pin
+  vout=(sensorValue*5.0)/1023; //Converts the value to a number out of 5V
+  vin=vout/(r2/(r1+r2)); //And uses voltage division to find the Voltage entering the circuit
+
+  Serial.println(vin); //Prints the voltage
+  //delay(100); 
+}
