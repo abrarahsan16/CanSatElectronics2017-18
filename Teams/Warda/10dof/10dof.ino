3d28cc8f04f1314ad43005ad9248fc041a175428
Wnoman <33441210+Wnoman@users.noreply.github.com>
2018-02-06 18:44:23 -0500

    Add files via upload

---
+++ Teams/Warda/10dof/10dof.ino
@@ -0,0 +1,258 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <HMC5883L.h>
+#include <MPU6050.h>
+
+//Variables
+int acc_error = 0;                       //We use this variable to only calculate once the Acc data error
+float rad_to_deg = 180 / 3.141592654;    //This value is for pasing from radians to degrees values
+float Acc_rawX, Acc_rawY, Acc_rawZ;    //Here we store the raw data read
+float Acc_angle_x, Acc_angle_y, Acc_angle_z;          //Here we store the angle value obtained with Acc data
+float Acc_angle_error_x, Acc_angle_error_y, Acc_angle_error_z; //Here we store the initial Acc data error
+float elapsedTime, time, timePrev;        //Variables for time control
+
+// Timers
+unsigned long timer = 0;
+float timeStep = 0.01;
+
+// Pitch, Roll and Yaw values
+float pitch = 0;
+float roll = 0;
+float yaw = 0;
+
+MPU6050 mpu;
+Adafruit_BMP085 bmp;
+HMC5883L compass;
+
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+
+    Serial.println("Initialize HMC5883L");
+    while (!compass.begin())
+    {
+      Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
+     
+    }
+
+    // Set measurement range
+    compass.setRange(HMC5883L_RANGE_1_3GA);
+
+    // Set measurement mode
+    compass.setMeasurementMode(HMC5883L_CONTINOUS);
+
+    // Set data rate
+    compass.setDataRate(HMC5883L_DATARATE_30HZ);
+
+    // Set number of samples averaged
+    compass.setSamples(HMC5883L_SAMPLES_8);
+
+    // Set calibration offset. See HMC5883L_calibration.ino
+    compass.setOffset(0, 0);
+
+    // Initialize MPU6050
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(1000);
+  }
+  
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  mpu.setThreshold(3);
+  }
+
+  Wire.begin();                           //begin the wire comunication
+
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68)
+  Wire.write(0x6B);                       //make the reset (place a 0 into the 6B register)
+  Wire.write(0x00);
+  Wire.endTransmission(true);             //end the transmission
+
+  Wire.beginTransmission(0x68);           //Start communication with the address found during search.
+  Wire.write(0x1C);                       //We want to write to the ACCEL_CONFIG register
+  Wire.write(0x10);                       //Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.endTransmission(true);
+  /*Here we calculate the acc data error before we start the loop
+     I make the mean of 200 values, that should be enough*/
+  if (acc_error == 0)
+  {
+    for (int a = 0; a < 200; a++)
+    {
+      Wire.beginTransmission(0x68);
+      Wire.write(0x3B);                       //Ask for the 0x3B register- correspond to AcX
+      Wire.endTransmission(false);
+      Wire.requestFrom(0x68, 6, true);
+
+      Acc_rawX = (Wire.read() << 8 | Wire.read()) / 4096.0 ; //each value needs two registres
+      Acc_rawY = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
+      Acc_rawZ = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
+
+
+      /*---X---*/
+      Acc_angle_error_x = Acc_angle_error_x + ((atan((Acc_rawY) / sqrt(pow((Acc_rawX), 2) + pow((Acc_rawZ), 2))) * rad_to_deg));
+      /*---Y---*/
+      Acc_angle_error_y = Acc_angle_error_y + ((atan(-1 * (Acc_rawX) / sqrt(pow((Acc_rawY), 2) + pow((Acc_rawZ), 2))) * rad_to_deg));
+      /*---Z---*/
+      Acc_angle_error_z = Acc_angle_error_z + ((atan(sqrt(pow((Acc_rawY), 2) + pow((Acc_rawX), 2) / (Acc_rawZ)) * rad_to_deg)));
+      if (a == 199)
+      {
+        Acc_angle_error_x = Acc_angle_error_x / 200;
+        Acc_angle_error_y = Acc_angle_error_y / 200;
+        Acc_angle_error_z = Acc_angle_error_z / 200;
+        acc_error = 1;
+      }
+    }
+  }//end of error calculation
+
+  Wire.begin();                           //begin the wire comunication
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68)
+  Wire.write(0x6B);                       //make the reset (place a 0 into the 6B register)
+  Wire.write(0x00);
+  Wire.endTransmission(true);             //end the transmission
+
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68)
+  Wire.write(0x1B);                       //We want to write to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10);                       //Set the register bits as 00010000 (1000dps full scale)
+  Wire.endTransmission(true);             //End the transmission with the gyro
+
+  //Serial.begin(9600);                     //Remember to set this same baud rate to the serial monitor
+  time = millis();                        //Start counting time in milliseconds
+
+
+
+  }
+
+void loop() {
+
+
+
+  /*Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+ Real altitude below*/   
+  
+ // Serial.print("");
+  Serial.print(bmp.readAltitude(101500));
+  Serial.print(" meters,");
+
+  /*Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+ Pressure at sea level bwlow*/
+  Serial.print(" ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.print(" Pa,");
+
+  Serial.print(" ");
+  Serial.print(bmp.readTemperature());
+  Serial.print(" *C,");
+
+
+
+ 
+
+  Vector norm = compass.readNormalize();
+
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+
+  // Set declination angle on your location and fix heading
+  // You can find your declination on: http://magnetic-declination.com/
+  // (+) Positive or (-) for negative
+  // For Bytom / Poland declination angle is 4'26E (positive)
+  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
+  float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / M_PI);
+  heading += declinationAngle;
+
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0)
+  {
+    heading += 2 * PI;
+  }
+
+  if (heading > 2 * PI)
+  {
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  float headingDegrees = heading * 180 / M_PI;
+
+  // Output
+  /*Serial.print(" Heading = ");
+  Serial.print(heading);
+  Serial.print(" Degress = ");
+  Serial.print(headingDegrees);
+  Serial.println();
+*/
+  //////////////////////////////////////Acc read/////////////////////////////////////
+
+  Wire.beginTransmission(0x68);     //begin, Send the slave adress (in this case 68)
+  Wire.write(0x3B);                 //Ask for the 0x3B register- correspond to AcX
+  Wire.endTransmission(false);      //keep the transmission and next
+  Wire.requestFrom(0x68, 6, true);  //We ask for next 6 registers starting withj the 3B
+  /*We have asked for the 0x3B register. The IMU will send a brust of register.
+    The amount of register to read is specify in the requestFrom function.
+    In this case we request 6 registers. Each value of acceleration is made out of
+    two 8bits registers, low values and high values. For that we request the 6 of them
+    and just make then sum of each pair. For that we shift to the left the high values
+    register (<<) and make an or (|) operation to add the low values.
+    If we read the datasheet, for a range of+-8g, we have to divide the raw values by 4096*/
+  Acc_rawX = (Wire.read() << 8 | Wire.read()) / 4096.0 ; //each value needs two registres
+  Acc_rawY = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
+  Acc_rawZ = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
+  /*Now in order to obtain the Acc angles we use euler formula with acceleration values
+    after that we substract the error value found before*/
+  /*---X---*/
+  Acc_angle_x = (atan((Acc_rawY) / sqrt(pow((Acc_rawX), 2) + pow((Acc_rawZ), 2))) * rad_to_deg) - Acc_angle_error_x;
+  /*---Y---*/
+  Acc_angle_y = (atan(-1 * (Acc_rawX) / sqrt(pow((Acc_rawY), 2) + pow((Acc_rawZ), 2))) * rad_to_deg) - Acc_angle_error_y;
+  /*---Z---*/
+  Acc_angle_z = (atan(sqrt (pow((Acc_rawY), 2) + pow((Acc_rawX), 2)) / (Acc_rawZ)) * rad_to_deg) - Acc_angle_error_z;
+
+  
+  //tiltx
+  Serial.print(Acc_angle_x);
+  Serial.print(",");
+
+  //tilty
+  Serial.print(" ");
+  Serial.print(Acc_angle_y);
+  Serial.print(",");
+
+  //tiltz
+  Serial.print(" ");
+  Serial.print(Acc_angle_z);
+  Serial.print (",");
+
+  timePrev = time;                        // the previous time is stored before the actual time read
+  time = millis();                        // actual time read
+  elapsedTime = (time - timePrev) / 1000; //divide by 1000 in order to obtain seconds
+
+ timer = millis();
+
+  // Read normalized values
+  Vector nor = mpu.readNormalizeGyro();
+
+  // Calculate Pitch, Roll and Yaw
+  pitch = pitch + nor.YAxis * timeStep;
+  roll = roll + nor.XAxis * timeStep;
+  yaw = yaw + nor.ZAxis * timeStep;
+
+  // Output raw
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.print(roll);  
+  Serial.print(" Yaw = ");
+  Serial.println(yaw);
+
+  delay(1000);
+}
