1caf9c54d8bed51285926759da4ebc81e6864648
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2018-03-12 19:29:12 -0400

    Camera

--- Flight Software/DropTest_V3/FSW_V3/Camera.ino
+++ Flight Software/DropTest_V3/FSW_V3/Camera.ino
@@ -1,1 +1,22 @@
 //Camera
+void setupCamera()
+{
+  pinMode(trig, OUTPUT);
+}
+
+void cameraON()
+{
+  //Turns camera on
+  digitalWrite(trig, HIGH);
+}
+
+void videoON()
+{
+  //Starts recording
+  digitalWrite(trig, LOW);
+}
+
+void videoOFF()
+{
+  digitalWrite(trig, HIGH);
+}
