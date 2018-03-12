aa86c044d94929c37d95b89eb55aa3d2eaca8151
BASThomasA <35878785+BASThomasA@users.noreply.github.com>
2018-03-12 19:43:17 -0400

    Add files via upload

--- Flight Software/DropTest_V3/FSW_V3/Voltage.ino
+++ Flight Software/DropTest_V3/FSW_V3/Voltage.ino
@@ -1,4 +1,10 @@
 //Voltage
+
+float vi; //Value of voltage supplied
+float vo; //Value of voltage read from Arduino
+float r1 = 10000; //Value of resistor 1
+float r2 = 10000; //Value of resistor 2
+
 void setupVoltage()
 {
   pinMode(A0, INPUT);
@@ -6,6 +12,16 @@ void setupVoltage()
 
 void callVoltage()
 {
+  int sensorValue = analogRead(A0);
   
+  // Converts the analog reading to a voltage between 0 - 5V
+  //Only applicable as long as the voltage does not exceed 10V
+  float vo = sensorValue * (5.0 / 1023.0);
+
+  //Calculates vi using voltage division
+  float vi = (vo * (r1 + r2)) / (r2); 
+  
+  //printing out vi
+  //Serial.println(vi);
 }
 
