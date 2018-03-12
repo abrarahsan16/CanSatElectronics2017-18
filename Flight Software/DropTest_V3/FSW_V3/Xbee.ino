1ba8c1ebf2c4cb58d9be1ee278566a9364c38e31
jackyluo123 <35754231+jackyluo123@users.noreply.github.com>
2018-03-12 19:28:17 -0400

    Add files via upload

---
+++ Flight Software/DropTest_V3/FSW_V3/Xbee.ino
@@ -0,0 +1,26 @@
+// Xbee
+void setupRadio()
+{
+  radio.begin(9600);
+  
+  //Pins should be declared in main file
+  //SoftwareSerial XBee(2, 3); // RX, TX
+}
+
+void radioTransmit()
+{
+  //Send the TeleArray array values to the ground station
+  for (int index = 0; index < sizeof(TeleArray); index++)
+  {
+    XBee.write (TeleArray[index]);
+  }
+    
+  if (XBee.available())
+  { 
+    // If data comes in from XBee, change a certain variable
+    
+  }
+}
+
+
+
